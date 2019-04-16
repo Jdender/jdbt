80fead503ef7925f205f13378df7ae2ee44ff435
Jdender~ <jdenderplays@gmail.com>
2019-04-16 14:55:41 -0400

    Add the rest of the tests

--- src/tests.rs
+++ src/tests.rs
@@ -15,3 +15,33 @@ fn encode_decode_test_helper(tag: Tag) {
 fn byte() {
     encode_decode_test_helper(Tag::Byte(1))
 }
+
+#[test]
+fn short() {
+    encode_decode_test_helper(Tag::Short(1))
+}
+
+#[test]
+fn int() {
+    encode_decode_test_helper(Tag::Int(1))
+}
+
+#[test]
+fn long() {
+    encode_decode_test_helper(Tag::Long(1))
+}
+
+#[test]
+fn float() {
+    encode_decode_test_helper(Tag::Float(1.0))
+}
+
+#[test]
+fn double() {
+    encode_decode_test_helper(Tag::Double(1.0))
+}
+
+#[test]
+fn string() {
+    encode_decode_test_helper(Tag::String("wew".to_owned()))
+}
